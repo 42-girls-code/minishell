c1b4e1aea273d5cccf495696d1bd46c6394ffe7d
IngridLemos <ingridlc42@gmail.com>
2026-03-13 11:52:23 -0300

    create builtin_pwd

--- src/built-in/built_in.c
+++ src/built-in/built_in.c
@@ -6,7 +6,7 @@
 /*   By: ingrid <ingrid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 13:30:53 by ingrid            #+#    #+#             */
-/*   Updated: 2026/03/13 10:22:51 by ingrid           ###   ########.fr       */
+/*   Updated: 2026/03/13 11:48:48 by ingrid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,19 @@ int	builtin_echo(char **args, t_envp *env_list)
 	}
 	if (!no_newline)
 		write(1, "\n", 1);
+	return (0);
+}
+
+int	builtin_pwd(void)
+{
+	char	pwd[PATH_MAX];
+
+	if (!getcwd(pwd, PATH_MAX))
+	{
+		ft_putstr_fd("pwd: getcwd impossible\n", 2);
+		return (1);
+	}
+	ft_putstr_fd(pwd , 1);
+	ft_putstr_fd("\n", 1);
 	return (0);
 }
