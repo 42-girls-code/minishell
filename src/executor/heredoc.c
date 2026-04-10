79d28994f34abf541c4e604e30a78567e0e4a714
Cris <criss.suominsky@gmail.com>
2026-04-10 17:44:24 -0300

    correcao de erro em heredoc signals, e do ctrl+c

--- src/executor/heredoc.c
+++ src/executor/heredoc.c
@@ -6,19 +6,21 @@
 /*   By: cris_sky <cris_sky@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 15:46:20 by ilemos-c          #+#    #+#             */
-/*   Updated: 2026/04/10 15:41:05 by cris_sky         ###   ########.fr       */
+/*   Updated: 2026/04/10 17:41:18 by cris_sky         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "executor.h"
 #include "minishell.h"
 #include <readline/readline.h>
-#include "signal.h"
+#include <signal.h>
+#include <sys/wait.h>
 
 static int	handle_heredoc(t_ast *cmd, t_minishell *shell);
 static int	read_heredoc(int write_fd, char *delimiter, t_minishell *shell);
-static void	handle_sigint_heredoc(int sig);
-static void	setup_heredoc_signals(void);
+static void	run_heredoc_child(int write_fd, char *delimiter);
+static void	child_sigint_handler(int sig);
 
 int	prepare_heredocs(t_ast *node, t_minishell *shell)
 {
@@ -43,10 +45,7 @@ static int	handle_heredoc(t_ast *cmd, t_minishell *shell)
 	if (!cmd->heredoc_delim)
 		return (0);
 	if (pipe(fd) < 0)
-	{
-		perror("pipe");
-		return (1);
-	}
+		return (perror("pipe"), 1);
 	if (read_heredoc(fd[1], cmd->heredoc_delim, shell))
 	{
 		close(fd[0]);
@@ -58,56 +57,66 @@ static int	handle_heredoc(t_ast *cmd, t_minishell *shell)
 	return (0);
 }
 
-static int	read_heredoc(int write_fd, char *delimiter, t_minishell *shell)
+static void	child_sigint_handler(int sig)
 {
-	char	*line;
+	(void)sig;
+	write(1, "\n", 1);
+	exit(130);
+}
 
-	setup_heredoc_signals();
+static void	run_heredoc_child(int write_fd, char *delimiter)
+{
+	struct sigaction	sa;
+	char				*line;
+
+	sa.sa_handler = child_sigint_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);
+	signal(SIGQUIT, SIG_IGN);
 	while (1)
 	{
 		line = readline("> ");
-		if (g_signal == SIGINT)
-		{
-			g_signal = 0;
-			shell->last_status = 130;
-			if (line)
-				free(line);
-			setup_signals();
-			return (1);
-		}
 		if (!line)
 		{
-			setup_signals();
-			return (0);
+			ft_putstr_fd(
+				"warning: here-document delimited by end-of-file\n", 2);
+			exit(0);
 		}
 		if (ft_strcmp(line, delimiter) == 0)
 		{
 			free(line);
-			break ;
+			exit(0);
 		}
 		write(write_fd, line, ft_strlen(line));
 		write(write_fd, "\n", 1);
 		free(line);
 	}
-	setup_signals();
-	return (0);
 }
 
-static void	handle_sigint_heredoc(int sig)
+static int	read_heredoc(int write_fd, char *delimiter, t_minishell *shell)
 {
-	(void)sig;
-	g_signal = SIGINT;
-	write(1, "\n", 1);
-	rl_done = 1;
-}
+	pid_t	pid;
+	int		status;
 
-static void	setup_heredoc_signals(void)
-{
-	struct sigaction	sa;
-
-	sa.sa_handler = handle_sigint_heredoc;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGINT, &sa, NULL);
+	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
+	pid = fork();
+	if (pid < 0)
+	{
+		setup_signals();
+		return (perror("fork"), 1);
+	}
+	if (pid == 0)
+		run_heredoc_child(write_fd, delimiter);
+	close(write_fd);
+	waitpid(pid, &status, 0);
+	g_signal = 0;
+	setup_signals();
+	if (WIFEXITED(status) && WEXITSTATUS(status) == 130)
+	{
+		shell->last_status = 130;
+		return (1);
+	}
+	return (0);
 }
--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ingrid <ingrid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cris_sky <cris_sky@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 20:19:24 by ingrid            #+#    #+#             */
-/*   Updated: 2026/04/02 15:44:30 by ingrid           ###   ########.fr       */
+/*   Updated: 2026/04/10 17:19:10 by cris_sky         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,12 @@ static void	init_t_minishell(t_minishell *shell, char *envp[])
 	shell->last_status = 0;
 	shell->env = init_env(envp);
 }
-
+ 
 int	main(int argc, char *argv[], char *envp[])
 {
 	char		*line;
 	t_minishell	shell;
-
+ 
 	(void)argc;
 	(void)argv;
 	init_t_minishell(&shell, envp);
@@ -55,14 +55,23 @@ int	main(int argc, char *argv[], char *envp[])
 		line = readline("minishell$ ");
 		if (!line)
 			break ;
+		if (g_signal == SIGINT)
+		{
+			g_signal = 0;
+			free(line);
+			continue ;
+		}
 		if (!is_empty(line))
 		{
 			add_history(line);
 			handle_input(line, &shell);
 		}
+		if (g_signal == SIGINT)
+			g_signal = 0;
 		free(line);
 	}
 	rl_clear_history();
 	free_env_list(shell.env);
 	return (0);
 }
+ 
